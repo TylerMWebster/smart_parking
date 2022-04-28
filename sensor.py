8afb4cd64783acdd04f6495efd68b856a6b79e13
TylerMWebster <tylerwebster206@gmail.com>
2022-04-28 18:10:38 -0400

    Cleaned up class file

--- sensor.py
+++ sensor.py
@@ -3,15 +3,6 @@ import RPi.GPIO as GPIO
 import time
 from client import *
 
-device_id = 1
-pulse_duration = 0
-distance = 0
-park_thresh = 25
-dist = ''
-occupied = False
-prev_occupied = False
-stat_change = False
-
 class sensor():
     def __init__(self, device_id, trig_pin, echo_pin, sensor_thresh):
         self.device_id = device_id
@@ -76,52 +67,3 @@ class sensor():
 if __name__ == '__main__':
     sensor1 = sensor(1,7,11,25)
     sensor1.start()
-
-# try:
-#     GPIO.setmode(GPIO.BOARD)
-
-#     PIN_TRIGGER = 7
-#     PIN_ECHO = 11
-
-#     GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-#     GPIO.setup(PIN_ECHO, GPIO.IN)
-
-#     GPIO.output(PIN_TRIGGER, GPIO.LOW)
-#     while True:
-#         prev_occupied = occupied
-#         #print("Waiting for sensor to settle")
-
-#         time.sleep(.1)
-
-#         #print("Calculating distance")
-
-#         GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-
-#         time.sleep(0.00001)
-
-#         GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-#         while GPIO.input(PIN_ECHO)==0:
-#             pulse_start_time = time.time()
-#         while GPIO.input(PIN_ECHO)==1:
-#             pulse_end_time = time.time()
-
-#         pulse_duration = pulse_end_time - pulse_start_time
-#         distance = round(pulse_duration * 17150, 2)
-#         #print("Distance:",distance,"cm")
-
-#         if (distance < park_thresh):
-#             occupied = True
-#         else:
-#             occupied = False
-        
-#         if occupied != prev_occupied:
-#             #print(f'Parking status change to {occupied}')
-#             post_status(device_id, occupied)
-        
-
-# except:
-#     print('fail')
-
-# finally:
-#     GPIO.cleanup()
